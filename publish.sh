1ae8db9b5d5fe7ce2ad946c2a45a314cea5b7772
Dmitry Fedosov <dimafeng@gmail.com>
2018-01-02 11:01:30 -0500

    Release mechanism update

--- publish.sh
+++ publish.sh
@@ -8,18 +8,18 @@ if [ version != "y" ]; then
    echo "${fileTail}" >> gradle.properties
 fi
 
-#scalaVersions="2.11.8 2.12.3"
-#
-#for version in $scalaVersions
-#do
-#    printf "\n========Run tests for scala $version==========\n"
-#    ./gradlew clean -Pscala=$version test integrationTest
-#done
-#
-#for version in $scalaVersions
-#do
-#    printf "\n========Upload archives for scala $version==========\n"
-#    ./gradlew clean -Pscala=$version uploadArchives
-#done
-#
-#./gradlew release
+scalaVersions="2.11.8 2.12.3"
+
+for version in $scalaVersions
+do
+    printf "\n========Run tests for scala $version==========\n"
+    ./gradlew clean -Pscala=$version test integrationTest
+done
+
+for version in $scalaVersions
+do
+    printf "\n========Upload archives for scala $version==========\n"
+    ./gradlew clean -Pscala=$version uploadArchives
+done
+
+./gradlew release
