5592304de39d13b8546d150a11c2b74bd196043c
Dmitry Fedosov <dimafeng@gmail.com>
2018-01-06 22:53:20 -0500

    Scala 2.10 support

--- publish.sh
+++ publish.sh
@@ -11,7 +11,7 @@ if [ version != "y" ]; then
    git push
 fi
 
-scalaVersions="2.11.8 2.12.3"
+scalaVersions="2.10.7 2.11.8 2.12.3"
 
 for version in $scalaVersions
 do
@@ -19,10 +19,10 @@ do
     ./gradlew clean -Pscala=$version test integrationTest
 done
 
-for version in $scalaVersions
-do
-    printf "\n========Upload archives for scala $version==========\n"
-    ./gradlew clean -Pscala=$version uploadArchives
-done
-
-./gradlew release
+#for version in $scalaVersions
+#do
+#    printf "\n========Upload archives for scala $version==========\n"
+#    ./gradlew clean -Pscala=$version uploadArchives
+#done
+#
+#./gradlew release
