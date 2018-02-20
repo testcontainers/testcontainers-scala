c5b4391e4e70cef193d7e6e0caedf95747e431c5
Marco Da Col <marco.dacol@ext.contactlab.com>
2018-02-20 18:20:19 +0100

    made mysql container a bit more configurable. Now dbname, mysqlversion, user and password can be configured

--- src/main/scala/com/dimafeng/testcontainers/MySQLContainer.scala
+++ src/main/scala/com/dimafeng/testcontainers/MySQLContainer.scala
@@ -2,10 +2,28 @@ package com.dimafeng.testcontainers
 
 import org.testcontainers.containers.{MySQLContainer => OTCMySQLContainer}
 
-class MySQLContainer(configurationOverride: Option[String] = None) extends SingleContainer[OTCMySQLContainer[_]] {
+class MySQLContainer(configurationOverride: Option[String] = None,
+                     mysqlImageVersion: Option[String] = None,
+                     databaseName: Option[String] = None,
+                     mysqlUsername: Option[String] = None,
+                     mysqlPassword: Option[String] = None)
+    extends SingleContainer[OTCMySQLContainer[_]] {
+  /*
+  def this(configurationOverride: Option[String] = None) {
+    this(configurationOverride, None, None)
+  }*/
 
-  type OTCContainer = OTCMySQLContainer[T] forSome {type T <: OTCMySQLContainer[T]}
-  override val container: OTCContainer = new OTCMySQLContainer()
+  type OTCContainer = OTCMySQLContainer[T] forSome {
+    type T <: OTCMySQLContainer[T]
+  }
+  override val container: OTCContainer = mysqlImageVersion
+    .map(new OTCMySQLContainer(_))
+    .getOrElse(new OTCMySQLContainer())
+
+  databaseName.map(container.withDatabaseName)
+  mysqlUsername.map(container.withUsername)
+  mysqlPassword.map(container.withPassword)
+
   configurationOverride.foreach(container.withConfigurationOverride)
 
   def driverClassName: String = container.getDriverClassName
@@ -17,8 +35,19 @@ class MySQLContainer(configurationOverride: Option[String] = None) extends Singl
   def testQueryString: String = container.getTestQueryString
 
   def username: String = container.getUsername
+
 }
 
 object MySQLContainer {
-  def apply(configurationOverride: String = null): MySQLContainer = new MySQLContainer(Option(configurationOverride))
+  def apply(configurationOverride: String = null,
+            mysqlImageVersion: String = null,
+            databaseName: String = null,
+            username: String = null,
+            password: String = null): MySQLContainer =
+    new MySQLContainer(Option(configurationOverride),
+                       Option(mysqlImageVersion),
+                       Option(databaseName),
+                       Option(username),
+                       Option(password))
+
 }
