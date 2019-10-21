a59f3105d1e890bc55c30eba75d8cd41f0278036
Dmitry Fedosov <dimafeng@gmail.com>
2019-10-21 00:03:36 -0400

    Revert: Attempt to fix test (c35aff6e)

--- test-framework/scalatest/src/test/scala/com/dimafeng/testcontainers/integration/ComposeSpec.scala
+++ test-framework/scalatest/src/test/scala/com/dimafeng/testcontainers/integration/ComposeSpec.scala
@@ -8,17 +8,17 @@ import org.scalatest.FlatSpec
 class ComposeSpec extends FlatSpec with ForAllTestContainer {
   override val container = DockerComposeContainer(
     new File(getClass.getClassLoader.getResource("docker-compose.yml").getPath),
-    Seq(ExposedService("redis", 6379))
+    Seq(ExposedService("redis_1", 6379))
   )
 
   "DockerComposeContainer" should "retrieve non-0 port for any of services" in {
-    assert(container.getServicePort("redis", 6379) > 0)
+    assert(container.getServicePort("redis_1", 6379) > 0)
   }
 }
 
 class ComposeSpecWithImplicitConversions extends ComposeSpec {
   override val container = DockerComposeContainer(
     Seq(new File(getClass.getClassLoader.getResource("docker-compose.yml").getPath)),
-    exposedServices = Seq(ExposedService("redis", 6379))
+    exposedServices = Seq(ExposedService("redis_1", 6379))
   )
 }
