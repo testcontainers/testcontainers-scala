8b853a806927b594929543aec0308e07f7bab379
Mikhail I <ivmikhail@gmail.com>
2020-03-12 02:19:08 +0700

    test improvements

--- test-framework/scalatest/src/test/scala/com/dimafeng/testcontainers/integration/ComposeWaitingForSpec.scala
+++ test-framework/scalatest/src/test/scala/com/dimafeng/testcontainers/integration/ComposeWaitingForSpec.scala
@@ -1,26 +1,29 @@
 package com.dimafeng.testcontainers.integration
 
 import java.io.File
-import java.time.Duration
+import java.time.Duration.ofSeconds
 
-import com.dimafeng.testcontainers.{DockerComposeContainer, WaitingForService}
+import com.dimafeng.testcontainers.{DockerComposeContainer, ForAllTestContainer, WaitingForService}
 import org.scalatest.FlatSpec
 import org.testcontainers.containers.wait.strategy.Wait
 
-class ComposeWaitingForSpec extends FlatSpec {
-  "DockerComposeContainer" should "wait for service" in {
-    val container = DockerComposeContainer(
-      Seq(new File(getClass.getClassLoader.getResource("docker-compose.yml").getPath)),
-      waitingFor = Some(WaitingForService("redis", Wait.forLogMessage(".*Ready to accept connections\\n", 1)))
-    )
-    container.start() // blocks until successful or timeout
+class ComposeWaitingForSpec extends FlatSpec with ForAllTestContainer {
+  override val container = DockerComposeContainer(
+    Seq(new File(getClass.getClassLoader.getResource("docker-compose.yml").getPath)),
+    waitingFor = Some(WaitingForService("redis", Wait.forLogMessage(".*Ready to accept connections\\n", 1)))
+  )
 
+  "DockerComposeContainer" should "wait for service" in {
+    // container.start() should blocks until successful or timeout
     assert(container.getContainerByServiceName("redis_1").get.isRunning)
   }
+}
+
+class ComposeWaitingForWithTimeoutSpec extends FlatSpec {
 
   "DockerComposeContainer" should "throw exception when timeout occurs" in {
     val waitStrategy = Wait.forLogMessage("this is never happen", 1)
-      .withStartupTimeout(Duration.ofSeconds(5L))
+      .withStartupTimeout(ofSeconds(5L))
 
     val container = DockerComposeContainer(
       Seq(new File(getClass.getClassLoader.getResource("docker-compose.yml").getPath)),
@@ -29,6 +32,8 @@ class ComposeWaitingForSpec extends FlatSpec {
     val caught = intercept[RuntimeException] {
       container.start()
     }
+    container.stop()
+
     assert(caught.getMessage.contains("Timed out waiting for log output matching"))
   }
 }
