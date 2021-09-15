f304c44020b09784143dc7b20ea42d877471f7d2
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2021-09-15 16:45:28 +0200

    2.11 compile

--- modules/kafka/src/main/scala/com/dimafeng/testcontainers/SchemaRegistryContainer.scala
+++ modules/kafka/src/main/scala/com/dimafeng/testcontainers/SchemaRegistryContainer.scala
@@ -3,19 +3,21 @@ package com.dimafeng.testcontainers
 import org.testcontainers.containers.Network
 
 import scala.collection.JavaConverters._
+import org.testcontainers.containers.{GenericContainer => JavaGenericContainer}
 
 class SchemaRegistryContainer(network: Network, kafkaHost: String, confluentPlatformVersion: String, schemaPort: Int)
-  extends GenericContainer(
-    dockerImage = s"confluentinc/cp-schema-registry:$confluentPlatformVersion",
-    exposedPorts = Seq(schemaPort),
-  ) {
+    extends GenericContainer(
+      new JavaGenericContainer(s"confluentinc/cp-schema-registry:$confluentPlatformVersion")
+    ) {
 
+  container.withExposedPorts(schemaPort)
   container.withNetwork(network)
   container.setEnv(
     List(
       s"SCHEMA_REGISTRY_HOST_NAME=${container.getHost}",
       s"SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS=$kafkaHost:9092"
-    ).asJava)
+    ).asJava
+  )
 
   def schemaUrl: String =
     s"http://${container.getHost}:${container.getMappedPort(schemaPort)}"
@@ -30,15 +32,16 @@ object SchemaRegistryContainer {
     network: Network,
     kafkaHost: String,
     confluentPlatformVersion: String = KafkaContainer.defaultTag,
-    schemaPort: Int = defaultSchemaPort): SchemaRegistryContainer =
+    schemaPort: Int = defaultSchemaPort
+  ): SchemaRegistryContainer =
     new SchemaRegistryContainer(network, kafkaHost, confluentPlatformVersion, schemaPort)
 
   case class Def(
     network: Network,
     kafkaHost: String,
     confluentPlatformVersion: String = KafkaContainer.defaultTag,
-    schemaPort: Int = defaultSchemaPort)
-    extends ContainerDef {
+    schemaPort: Int = defaultSchemaPort
+  ) extends ContainerDef {
 
     override type Container = SchemaRegistryContainer
 
@@ -47,4 +50,3 @@ object SchemaRegistryContainer {
   }
 
 }
-
