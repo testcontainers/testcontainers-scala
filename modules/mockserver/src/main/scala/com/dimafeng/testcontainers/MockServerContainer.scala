654f1bf94e2aedd24f19e51a3653cfdd511f48d9
jczuchnowski <jakub.czuchnowski@gmail.com>
2022-05-31 13:02:48 +0200

    Fix MockServer container tag

--- modules/mockserver/src/main/scala/com/dimafeng/testcontainers/MockServerContainer.scala
+++ modules/mockserver/src/main/scala/com/dimafeng/testcontainers/MockServerContainer.scala
@@ -1,12 +1,16 @@
 package com.dimafeng.testcontainers
 
 import org.testcontainers.containers.{MockServerContainer => JavaMockServerContainer}
+import org.testcontainers.utility.DockerImageName
 
 case class MockServerContainer(
   version: String = MockServerContainer.defaultVersion
 ) extends SingleContainer[JavaMockServerContainer] {
 
-  override val container: JavaMockServerContainer = new JavaMockServerContainer(version)
+  override val container: JavaMockServerContainer = 
+    new JavaMockServerContainer(
+      MockServerContainer.defaultImageName.withTag(s"mockserver-$version")
+    )
 
   def endpoint: String = container.getEndpoint
 
@@ -15,10 +19,12 @@ case class MockServerContainer(
 
 object MockServerContainer {
 
-  val defaultVersion = JavaMockServerContainer.VERSION
+  private[testcontainers] final val defaultImageName = DockerImageName.parse("mockserver/mockserver")
+
+  private[testcontainers] final val defaultVersion = "5.13.2"
 
   case class Def(
-    version: String = MockServerContainer.defaultVersion
+    version: String = defaultVersion
   ) extends ContainerDef {
 
     override type Container = MockServerContainer
