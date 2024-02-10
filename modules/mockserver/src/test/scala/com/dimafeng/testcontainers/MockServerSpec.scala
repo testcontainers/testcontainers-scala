526d793a332f7ca2d97812e7557f8fa402ce60a0
Roman Greshny <greshny@gmail.com>
2024-02-10 16:11:28 +0100

    fix test

--- modules/mockserver/src/test/scala/com/dimafeng/testcontainers/MockServerSpec.scala
+++ modules/mockserver/src/test/scala/com/dimafeng/testcontainers/MockServerSpec.scala
@@ -5,11 +5,13 @@ import org.mockserver.client.MockServerClient
 import org.mockserver.model.HttpRequest.request
 import org.mockserver.model.HttpResponse.response
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-import org.scalatest.matchers.should.Matchers.not.be
-import sttp.client3.{UriContext, basicRequest}
+import org.scalatest.matchers.should.Matchers
+import sttp.client3.{HttpURLConnectionBackend, UriContext, basicRequest}
 
-class MockServerSpec extends AnyFlatSpec with TestContainersForAll {
+class MockServerSpec
+    extends AnyFlatSpec
+    with TestContainersForAll
+    with Matchers {
   override type Containers = MockServerContainer
 
   override def startContainers(): MockServerContainer =
@@ -17,23 +19,20 @@ class MockServerSpec extends AnyFlatSpec with TestContainersForAll {
 
   "MockServer container" should "be started" in withContainers {
     mockServerContainer =>
+      val backend = HttpURLConnectionBackend()
+
       val mockServerClient = new MockServerClient(
         mockServerContainer.serverHost,
         mockServerContainer.serverPort
       )
 
       mockServerClient
-        .when(
-          request()
-            .withMethod("GET")
-            .withPath("/hello")
-        )
-        .respond(
-          response.withBody("world")
-        )
+        .when(request("/hello"))
+        .respond(response("world"))
 
       basicRequest
         .get(uri"${mockServerContainer.endpoint}/hello")
+        .send(backend)
         .body should be(Right("world"))
   }
 }
