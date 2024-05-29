e7717c094c4a3c18b007aa6353c18b8a33949ab0
Scala Steward <scala_steward@virtuslab.com>
2024-05-29 20:30:51 +0000

    Update neo4j-java-driver to 4.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   private val junitVersion = "4.13.2"
   private val munitVersion = "1.0.0-M7"
   private val mysqlConnectorVersion = "5.1.42"
-  private val neo4jConnectorVersion = "4.0.0"
+  private val neo4jConnectorVersion = "4.0.3"
   private val oracleDriverVersion = "21.3.0.0"
   private val cassandraDriverVersion = "4.0.1"
   private val postgresqlDriverVersion = "42.2.24"
