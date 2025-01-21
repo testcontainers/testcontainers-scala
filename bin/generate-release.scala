94ff7f4a7aa298f90f525285030838a9c4485f8f
Scala Steward <scala_steward@virtuslab.com>
2025-01-21 02:03:44 +0000

    Update decline to 2.5.0

--- bin/generate-release.scala
+++ bin/generate-release.scala
@@ -1,6 +1,6 @@
 //> using scala 3.6.2
 //> using dep "com.lihaoyi::os-lib:0.11.3"
-//> using dep "com.monovore::decline:2.4.1"
+//> using dep "com.monovore::decline:2.5.0"
 //> using dep "org.kohsuke:github-api:1.326"
 
 import scala.jdk.CollectionConverters._
