515d259e4208c3becf2aa4ba7b1217b49d931ace
Scala Steward <scala_steward@virtuslab.com>
2025-02-27 18:17:19 +0000

    Update google-cloud-pubsub to 1.137.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   private val sttpVersion = "3.3.14"
   private val firestoreConnectorVersion = "3.0.11"
   private val bigtableVersion = "2.45.0"
-  private val pubsubVersion = "1.116.4"
+  private val pubsubVersion = "1.137.1"
   private val redisTestcontainersVersion = "2.0.1"
   private val jedisVersion = "5.0.0"
   private val wireMockTestcontainersVersion = "1.0-alpha-13"
