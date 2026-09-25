e0b41656d3e71363f641e9cc82453ebef3d3f2fb
Scala Steward <scala_steward@virtuslab.com>
2026-09-25 01:18:15 +0000

    Update selenium-java to 4.49.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   }
 
   private val testcontainersVersion = "2.0.5"
-  private val seleniumVersion = "2.53.1"
+  private val seleniumVersion = "4.49.0"
   private val slf4jVersion = "2.0.20"
   private val scalaTestVersion = "3.2.9"
   private val scalaTestMockitoVersion = "3.2.9.0"
